e57abb4f4ab73cdf861c6fdc2145163c0af5c695
hazyala <heamin0603@naver.com>
2026-03-24 11:49:38 +0900

    URL

---
+++ 2026Example/src/test/java/pollytech/aisw/_026example/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pollytech.aisw._026example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
